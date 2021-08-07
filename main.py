23280ec405f2a31eb0c8cc4c315b9e4b37e2a8b0
Leomotors (Windows) <59821765+Leomotors@users.noreply.github.com>
2021-08-07 19:45:02 +0700

    it works!

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
-from PIL import Image
-import math
+import PIL.Image
+import pygame as pg
+from utils.ImgToASCII import ImgToASCII
+import cv2
+from tqdm import tqdm
 
 SETTINGS = {}
 
@@ -9,5 +12,77 @@ with open("config.json") as config:
 
 ASCII_CHARS = [x for x in SETTINGS["ASCIIArray"]]
 IMG_WIDTH = SETTINGS["ImageWidth"]
+SCREEN_RES = tuple(int(r) for r in SETTINGS["ScreenResolution"].split(' '))
+FONT_LOC = SETTINGS["fontName"]
+FONT_SIZE = SETTINGS["fontSize"]
+FPS = SETTINGS["fps"]
+
+pg.init()
+pg.display.set_caption("ASCII Art Visualizer")
+
+screen = pg.display.set_mode(SCREEN_RES)
+setfps = pg.time.Clock()
+font = pg.font.Font(FONT_LOC, FONT_SIZE)
+
+vidcap = cv2.VideoCapture('assets/ご注文はうさぎですか？？ ED2.mp4')
+
+video_data = []
 
 
+def process_frame(image):
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    img = PIL.Image.fromarray(img)
+    raw_data = ImgToASCII(img, ASCII_CHARS, IMG_WIDTH)
+    video_data.append(raw_data["data"])
+    global height, width
+    height = raw_data["dimension"][0]
+    width = raw_data["dimension"][1]
+
+
+success = True
+count = 1
+
+while True:
+    success, image = vidcap.read()
+    if not success:
+        break
+    process_frame(image)
+    print(f"Processed #{count} Frames")
+    count += 1
+
+
+ascii_dict = {}
+for c in ASCII_CHARS:
+    ascii_dict[c] = font.render(c, True, (255, 255, 255))
+
+
+def Visualize(pixels):
+    for index, data in enumerate(pixels):
+        row = index/width
+        col = index % width
+        rowpos = row*SCREEN_RES[0]/width
+        colpos = col*SCREEN_RES[1]/height
+        screen.blit(ascii_dict[data], (colpos, rowpos))
+
+
+frame = 0
+
+
+def Loop():
+    global frame
+    screen.fill((0, 0, 0))
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+
+    Visualize(video_data[frame])
+    frame += 1
+    if(frame >= len(video_data)):
+        frame = 0
+
+    pg.display.flip()
+    setfps.tick(FPS)
+
+
+while True:
+    Loop()
--- utils/ImgToASCII.py
+++ utils/ImgToASCII.py
@@ -6,12 +6,11 @@ def ImgToASCII(image: PIL.Image.Image, chars: str, target_width: int) -> dict:
     width, height = image.size
     target_height = int(target_width * height / width)
     resized_image = image.resize((target_width, target_height))
-    print(target_height, target_width)
-    resized_image.convert("L")
-    pixels = resized_image.getdata()
+    gray_image = resized_image.convert("L")
+    pixels = gray_image.getdata()
     ascii_string = "".join(
-        [chars[math.floor(pixel/(256/len(chars)))] for pixel in pixels])
+        [chars[math.floor((pixel/256)*len(chars))] for pixel in pixels])
     return {
         "data": [c for c in ascii_string],
-        "dimension" : (target_height, target_width)
+        "dimension": (target_height, target_width)
     }
