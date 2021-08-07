858eab0d07728ff99c9e5ebc24ec4a2d2305607a
Leomotors (Windows) <59821765+Leomotors@users.noreply.github.com>
2021-08-07 20:04:47 +0700

    yes

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pygame as pg
 from utils.ImgToASCII import ImgToASCII
 import cv2
 from tqdm import tqdm
+import moviepy.editor as mp
+
 
 SETTINGS = {}
 
@@ -14,8 +16,7 @@ ASCII_CHARS = [x for x in SETTINGS["ASCIIArray"]]
 IMG_WIDTH = SETTINGS["ImageWidth"]
 SCREEN_RES = tuple(int(r) for r in SETTINGS["ScreenResolution"].split(' '))
 FONT_LOC = SETTINGS["fontName"]
-FONT_SIZE = SETTINGS["fontSize"]
-FPS = SETTINGS["fps"]
+FONT_SIZE = round(SETTINGS["fontSizeAt120p"] * 120 / IMG_WIDTH)
 
 pg.init()
 pg.display.set_caption("ASCII Art Visualizer")
@@ -24,7 +25,16 @@ screen = pg.display.set_mode(SCREEN_RES)
 setfps = pg.time.Clock()
 font = pg.font.Font(FONT_LOC, FONT_SIZE)
 
-vidcap = cv2.VideoCapture('assets/ご注文はうさぎですか？？ ED2.mp4')
+VIDEO = "assets/ご注文はうさぎですか？？ ED2.mp4"
+vidcap = cv2.VideoCapture(VIDEO)
+vid_fps = vidcap.get(cv2.CAP_PROP_FPS)
+vid_length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+clip = mp.VideoFileClip(VIDEO)
+clip.audio.write_audiofile(r"temp/temp.mp3")
+
+music = pg.mixer.Sound("temp/temp.mp3")
+
 
 video_data = []
 
@@ -38,23 +48,19 @@ def process_frame(image):
     height = raw_data["dimension"][0]
     width = raw_data["dimension"][1]
 
-
-success = True
-count = 1
-
-while True:
+print("Processing Video...")
+for _ in tqdm(range(vid_length)):
     success, image = vidcap.read()
     if not success:
         break
     process_frame(image)
-    print(f"Processed #{count} Frames")
-    count += 1
+print("Process Video Done!")
 
-
+print("Processing ASCII Characters...")
 ascii_dict = {}
-for c in ASCII_CHARS:
+for c in tqdm(ASCII_CHARS):
     ascii_dict[c] = font.render(c, True, (255, 255, 255))
-
+print("Processing ASCII Characters Done!")
 
 def Visualize(pixels):
     for index, data in enumerate(pixels):
@@ -79,10 +85,12 @@ def Loop():
     frame += 1
     if(frame >= len(video_data)):
         frame = 0
+        music.play()
 
     pg.display.flip()
-    setfps.tick(FPS)
+    setfps.tick(vid_fps)
 
+music.play()
 
 while True:
     Loop()
